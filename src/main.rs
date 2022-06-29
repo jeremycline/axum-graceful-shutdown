d1c421aeea9990728a0ff5c4443a4bcfb5b64e70
Jeremy Cline <jeremy.cline@deepgram.com>
2022-06-29 15:02:00 -0400

    Read as fast as we can

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::SocketAddr, time::Duration};
+use std::{net::SocketAddr};
 
 use axum::{
     extract::ws::{WebSocket, WebSocketUpgrade},
@@ -7,9 +7,6 @@ use axum::{
     Router,
 };
 use axum_server::Handle;
-use futures_util::{SinkExt, StreamExt};
-use tokio::time::sleep;
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
 /// Sample application demonstrating open WebSockets don't contribute to the
 /// connection count and don't have an opportunity to shutdown gracefully.
@@ -18,8 +15,6 @@ async fn main() {
     let app = Router::new().route("/ws", get(ws_handler));
 
     let handle = Handle::new();
-
-    tokio::spawn(websocket_client(handle.clone()));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {}", addr);
@@ -32,40 +27,11 @@ async fn main() {
     println!("server shut down")
 }
 
-async fn graceful_shutdown(handle: Handle) {
-    handle.graceful_shutdown(Some(Duration::from_secs(30)));
-
-    loop {
-        println!("alive connections: {}", handle.connection_count());
-        sleep(Duration::from_secs(1)).await;
-    }
-}
-
 async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
-    ws.on_upgrade(handle_socket)
+    ws.on_upgrade(websocket_reader)
 }
 
-async fn handle_socket(mut socket: WebSocket) {
-    while let Some(Ok(msg)) = socket.recv().await {
-        if let axum::extract::ws::Message::Text(msg) = msg {
-            println!("Got {}", msg)
-        }
-    }
-}
-
-async fn websocket_client(handle: Handle) {
-    let url = url::Url::parse("ws://127.0.0.1:3000/ws").unwrap();
-
-    let (ws_stream, _) = connect_async(url).await.unwrap();
-    let (mut writer, _reader) = ws_stream.split();
-
-    tokio::spawn(graceful_shutdown(handle.clone()));
-
-    loop {
-        tokio::time::sleep(Duration::from_secs(1)).await;
-        writer
-            .send(Message::Text("beep".to_string()))
-            .await
-            .unwrap();
+async fn websocket_reader(mut socket: WebSocket) {
+    while let Some(Ok(_)) = socket.recv().await {
     }
 }
